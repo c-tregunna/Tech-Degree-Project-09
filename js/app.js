91d9ff4c7661c3ec1e6ee1f6c30afc3d9c035764
Claire Tregunna <c.tregunna1980@gmail.com>
2020-05-15 18:23:10 +0100

    basic files

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// Header carosel
+
+// $(document).ready(function(){
+//     $('.carosel').slick({
+//         $('.single-item').slick();
+//     });
+//   });
+
+$('.slides').slick({
+    dots: true,
+    autoplay: true,
+    arrows: false
+  });
