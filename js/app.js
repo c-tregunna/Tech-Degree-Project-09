f4601ce07b5cb1bdcf586116bf7f00898938e835
Claire Tregunna <c.tregunna1980@gmail.com>
2020-06-19 10:26:31 +0100

    new site start

--- js/app.js
+++ js/app.js
@@ -1,15 +1,32 @@
-// Header carosel
 
-// $(document).ready(function(){
-//     $('.carosel').slick({
-//         $('.single-item').slick();
-//     });
-//   });
+// Menu
+const menuIcon = document.getElementById("menuIcon");
+const mainNav = document.querySelector(".main-nav");
+const closeBtn = document.querySelector(".closebtn");
+const menu = document.getElementById("myNav");
 
-$('.slides').slick({
-    dots: true,
-    autoplay: true,
-    arrows: false,
-    autoplaySpeed: 4000,
-    fade: true
-  });
+
+menuIcon.addEventListener('click', () => { // on click of menu icon hide the main-nav background and icon
+    menuIcon.style.display = "none";
+    mainNav.style.backgroundColor = "transparent";
+    menu.style.width = "100%";
+    menu.style.transition = "all .8s";
+})
+
+closeBtn.addEventListener('click', () => { // on click of close button, show main-nav and menu button
+    menuIcon.style.display = "block";
+    mainNav.style.backgroundColor = "rgba(0, 0, 0, 0.9)";
+    menu.style.width = "0";
+    menu.style.transition = "all .8s";
+})
+
+
+
+// function openNav() {
+//     document.getElementById("myNav").style.width = "100%";
+//   }
+
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
