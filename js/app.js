f612ad78c16a6c1adf21c5db7213436de23dbb91
Claire Tregunna <c.tregunna1980@gmail.com>
2020-07-05 13:35:12 +0100

    About section

--- js/app.js
+++ js/app.js
@@ -25,3 +25,31 @@ closeBtn.addEventListener('click', () => { // on click of close button, show mai
     menu.style.transition = "all .8s";
 });
 
+// Target the li's so on click the menu closes
+
+//----------------------
+// Skills Chart
+//----------------------
+
+var ctx = document.getElementById('mySkills').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'pie',
+
+    // The data for our dataset
+    data: {
+        labels: ['HTML', 'CSS', 'Javascript'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: ['#ccc5b9ff',
+            '#403d39ff',
+            '#eb5e28ff'],
+            data: [40, 40, 20]
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+    }
+});
+
